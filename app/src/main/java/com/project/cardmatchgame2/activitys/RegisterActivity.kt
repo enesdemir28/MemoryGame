e6789f7cf345f4a6be38776df8e4a2619f2e4ff2
EnesDemir28 <92634461+FrankWoodsx@users.noreply.github.com>
2023-05-22 13:43:10 +0300

    MemoryGame

---
+++ app/src/main/java/com/project/cardmatchgame2/activitys/RegisterActivity.kt
@@ -0,0 +1,14 @@
+package com.project.cardmatchgame2.activitys
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.project.cardmatchgame2.R
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+
+
+    }
+}
